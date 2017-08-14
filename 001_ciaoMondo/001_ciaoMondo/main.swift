23c76b42b460a6fd9c1a66d3d6ebd6114d0fbefc
ErmaC <martinofabio@libero.it>
2017-08-15 00:45:12 +0100

    simple hello, World

---
+++ 001_ciaoMondo/001_ciaoMondo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ciao Mondo
+//
+//  Created by Fabio Martino on 15/08/17.
+//  Copyright © 2017 ErmaC. All rights reserved.
+//
+
+import Foundation
+
+print("Ciao, Mondo!")
+
